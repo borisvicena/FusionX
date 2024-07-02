12c88a4c868f164da24c3039f0afefaf19ee5fa1
Boris <boris.vicena@gmail.com>
2024-07-02 21:25:37 +0200

    Removed border from titles in subpages

--- FusionX-App/src/components/Services.vue
+++ FusionX-App/src/components/Services.vue
@@ -4,14 +4,12 @@
 
 <template>
   <div class="min-h-screen flex flex-col justify-center">
-    <div
-      class="fixed w-full flex flex-wrap items-center justify-between mx-auto"
-    >
-      <div class="flex w-2/4 justify-center border">
+    <div class="fixed w-full flex flex-wrap items-center justify-between mx-auto">
+      <div class="flex w-2/4 justify-center">
         <h1 class="text-4xl text-white font-light">Services</h1>
       </div>
 
-      <div class="flex w-2/4 justify-center border">
+      <div class="flex w-2/4 justify-center">
         <h2 class="text-white">| photo |</h2>
       </div>
     </div>
