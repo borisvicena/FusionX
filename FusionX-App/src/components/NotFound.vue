08ebb731a7a4030af4899c71d435c5c4de5e6ef7
Boris <boris.vicena@gmail.com>
2024-07-05 14:16:17 +0200

    Small path detail changed

--- FusionX-App/src/components/NotFound.vue
+++ FusionX-App/src/components/NotFound.vue
@@ -5,7 +5,7 @@ import { RouterLink } from "vue-router";
 <template>
   <div class="min-h-screen flex flex-col items-center justify-center">
     <div class="dark:bg-secondary-gradient p-8 shadow-md border border-primary-pink w-96 rounded-xl text-center">
-      <img src="../../public/404.svg" alt="Not Found" />
+      <img src="/404.svg" alt="Not Found" />
       <h1 class="text-3xl font-bold mb-4 dark:text-white">Something went wrong!</h1>
       <p class="text-gray-500 mb-4">Page Not Found</p>
       <RouterLink to="/" class="text-blue-500">Go to Home</RouterLink>
